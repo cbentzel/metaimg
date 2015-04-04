2c287027b6bd8e28eb5b4c4b6c142d288ed775a6
Chris Bentzel <chris@bentzel.net>
2015-04-03 20:28:55 -0400

    Fix logging

---
+++ README.md
@@ -0,0 +1,1 @@
+A basic Chrome extension for providing metadata about images.
--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 // The onClicked callback function.
 function onClickHandler(info, tab) {
   console.log("It was selected.");
+  console.log(info);
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
-  console.log("Being installed.");
   var id = chrome.contextMenus.create({
     "type": "normal",
     "id": "metaimg",
     "title": "Image Metadata", 
     "contexts": ["image"]
   });
-  console.log(id);
 });
